bae7812ba3f2e6e3448c98b4cc81ece49b2c04d6
Eva María Gaitán Cerezo <emgaitan@stratio.com>
2022-11-07 08:26:55 +0100

    [SAAS-6968] Upgrade to Cypress 10.11.0 (#882)
    * [SAAS-6968] Upgrade to Cypress 10.11.0
    
    * Fix test extension
    
    * Fix cypress config
    
    * add ticket to changelog
    
    Co-authored-by: Alejandro Sanz <alejandrosanz@stratio.com>

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -4,14 +4,14 @@ const path = require('path');
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+    // `on` is used to hook into various events Cypress emits
+    // `config` is the resolved Cypress config
 
     const options = {
         outputRoot: config.projectRoot + '/target/',
-        specRoot: path.relative(config.fileServerFolder, config.integrationFolder),
+        specRoot: path.relative(config.fileServerFolder, 'cypress/e2e'),
         outputTarget: {
-              'executions|txt': 'txt',
+            'executions|txt': 'txt',
         },
         printLogsToConsole: 'always',
         printLogsToFile: 'always'
